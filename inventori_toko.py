cda743995ab38ab657a9833e74e9ed278e81afc7
andhikafarhan <andhikaf826@gmail.com>
2025-07-15 07:27:45 +0700

    inventori_toko.py

--- inventori_toko.py
+++ inventori_toko.py
@@ -36,3 +36,76 @@ def tambah_barang():
         print("Barang berhasil ditambahkan!")
     except ValueError:
         print("Stok harus berupa angka!")  # jika input bukan angka
+
+# Fungsi: Menampilkan semua barang di inventori
+def tampilkan_semua():
+    if not inventori:
+        print("Inventori kosong.")
+    else:
+        print("\n--- Daftar Barang ---")
+        for barang in inventori:
+            print(f"{barang['kode']} - {barang['nama']} ({barang['stok']})")
+
+
+# Fungsi: Mencari barang berdasarkan kode atau nama
+def cari_barang():
+    kata_kunci = input("Masukkan nama/kode barang: ").lower()
+    ditemukan = False
+    for barang in inventori:
+        # cek apakah kata kunci ada di kode atau di nama
+        if kata_kunci in barang["kode"].lower() or kata_kunci in barang["nama"].lower():
+            print(f"{barang['kode']} - {barang['nama']} ({barang['stok']})")
+            ditemukan = True
+    if not ditemukan:
+        print("Barang tidak ditemukan.")
+
+# Fungsi: Mengubah jumlah stok barang berdasarkan kode
+def update_stok():
+    kode = input("Masukkan kode barang yang ingin di-update: ").strip()
+    for barang in inventori:
+        if barang["kode"] == kode:
+            try:
+                baru = int(input("Masukkan jumlah stok baru: "))
+                barang["stok"] = baru
+                print("Stok berhasil diupdate!")
+                return
+            except ValueError:
+                print("Input tidak valid. Stok harus angka.")
+                return
+    print("Barang tidak ditemukan.")
+
+# Fungsi: Menghapus barang berdasarkan kode
+def hapus_barang():
+    kode = input("Masukkan kode barang yang ingin dihapus: ").strip()
+    for barang in inventori:
+        if barang["kode"] == kode:
+            inventori.remove(barang)
+            print("Barang berhasil dihapus!")
+            return
+    print("Barang tidak ditemukan.")
+
+
+# ----- Loop utama -----
+def main():
+    while True:
+        tampilkan_menu()
+        pilihan = input("Pilih menu (1-6): ").strip()
+        if pilihan == "1":
+            tambah_barang()
+        elif pilihan == "2":
+            tampilkan_semua()
+        elif pilihan == "3":
+            cari_barang()
+        elif pilihan == "4":
+            update_stok()
+        elif pilihan == "5":
+            hapus_barang()
+        elif pilihan == "6":
+            print("Terima kasih! Keluar program.")
+            break
+        else:
+            print("Pilihan tidak valid!")
+
+# Jalankan program
+if __name__ == "__main__":
+    main()
